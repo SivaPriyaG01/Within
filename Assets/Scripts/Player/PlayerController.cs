3d57ff8814acdda81f852b58c2cbec4410c76580
SivaPriyaG01 <sivapriyagarladinne@gmail.com>
2025-06-15 23:08:11 +0530

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private PlayerInput playerInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
